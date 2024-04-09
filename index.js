1ae614a5e4da4484b174930c1196e15d535d15f7
jeisson andres <jeissoncar97@gmail.com>
2024-04-09 02:06:38 -0500

    Cambio botones, responsive y tamanos

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const temaOscuro = ()=>{
-    document.querySelector('body').setAttribute("data-bs-theme", "dark")
+    body = document.querySelector('body')
+    body.setAttribute("data-bs-theme", "dark")
+    body.classList.remove("back")
+    navbar = document.querySelector('.navbar')
+    navbar.classList.remove("back2")
+    navbar.classList.add("backdark")
+
     document.querySelector('#dl-icon').setAttribute("class", "bi bi-sun-fill sun")
 }
 const temaClaro = ()=>{
     document.querySelector('body').setAttribute("data-bs-theme", "light")
     document.querySelector('#dl-icon').setAttribute("class", "bi bi-moon-fill moon")
+    body.classList.add("back")
+    navbar.classList.add("back2")
+    navbar.classList.remove("backdark")
+
+    
 }
 const cambiarTema = ()=>{
     document.querySelector('body').getAttribute("data-bs-theme") === "dark"?
