bc02d8052fdc9d9c0e795f5c66731edf5eaacfb2
jeisson andres <jeissoncar97@gmail.com>
2024-04-12 03:31:01 -0500

    Cambios

--- Cocteles/script-cocktail.js
+++ Cocteles/script-cocktail.js
@@ -1,25 +1,30 @@
 const contenedorporNombre = document.querySelectorAll(".contenedor-general.porNombre");
 const contenedorporLetra = document.querySelectorAll(".contenedor-general.porLetra");
 const contenedoraleatorio = document.querySelectorAll(".contenedor-general.aleatorio");
+const contenedorConAlcohol = document.querySelectorAll(".contenedor-general.conAlcohol");
 const liPorLetra = document.querySelector("#por-letra");
 const liPorNombre = document.querySelector("#por-nombre");
 const liAleatorio = document.querySelector("#aleatorio");
+const liConAlcohol = document.querySelecto("#con-alcohol");
 const liPL = document.querySelector("#por-letra li");
 const liPN = document.querySelector("#por-nombre li");
 const liA = document.querySelector("#aleatorio li");
+const liCA = document.querySelector("#con-alcohol li");
 const url1 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
 const url2 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const url3 = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+const url4 = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
 const letraInput = document.querySelector(".letra-coctel");
 const nombreInput = document.querySelector(".nombre-coctel");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
 const resultadosContainer1 = document.querySelector(".resultado1");
 const resultadosContainer2 = document.querySelector(".resultado2");
 const resultadosContainer3 = document.querySelector(".resultado3");
+const resultadosContainer4 = document.querySelector(".resultado4");
 let contador = 1
-
 
 
 liPorLetra.addEventListener("click", () => { 
@@ -27,7 +32,7 @@ liPorLetra.addEventListener("click", () => {
         element.classList.add("active");
         liPL.classList.add("active-a");
         liPN.classList.remove("active-a");
-        liA.classList.remove("active-a");
+        liCA.classList.remove("active-a");
     });
 
     contenedorporNombre.forEach(element => {
@@ -44,6 +49,7 @@ liPorNombre.addEventListener("click", () => {
         liPN.classList.add("active-a");
         liPL.classList.remove("active-a");
         liA.classList.remove("active-a");
+        liCA.classList.remove("active-a");
     });
     
     contenedorporLetra.forEach(element => {
@@ -55,6 +61,24 @@ liPorNombre.addEventListener("click", () => {
 
 });
 liAleatorio.addEventListener("click", () => {
+    contenedoraleatorio.forEach(element => {
+        element.classList.add("active");
+        liA.classList.add("active-a");
+        liPL.classList.remove("active-a");
+        liPN.classList.remove("active-a");
+        liCA.classList.remove("active-a");
+    });
+    
+    contenedorporLetra.forEach(element => {
+        element.classList.remove("active");
+
+    });
+    contenedorporNombre.forEach(element => {
+        element.classList.remove("active");
+    });
+    
+});
+liConAlcohol.addEventListener("click", () => {
     contenedoraleatorio.forEach(element => {
         element.classList.add("active");
         liA.classList.add("active-a");
@@ -81,6 +105,7 @@ btn2.addEventListener("click", ()=> {obtenerCocteles(url2,nombreInput,resultados
 nombreInput.addEventListener("keydown", function(event){
    if (event.key === "Enter") {
     obtenerCocteles(url2,nombreInput,resultadosContainer2);
+    console.log(typeof nombreInput);
    } 
 });
 
@@ -96,7 +121,6 @@ function obtenerCocteles(url2,input,resultadosContainer){
         .then(data => {
             
             const cocteles = data.drinks;
-            console.log(cocteles)
             let html = '';
             resultadosContainer.innerHTML = '';
             
@@ -133,7 +157,6 @@ function obtenerCoctelesAleatorio(url, resultadosContainer){
     .then(response => response.json())
     .then(data => {
         const cocteles = data.drinks;
-        console.log(cocteles.strDrink);
         let html = '';
         resultadosContainer.innerHTML = '';
         
