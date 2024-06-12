4b58d81b495f0168bbba65869b955d7e48ecbe3e
Jeisson Cardenas <jeissoncar97@gmail.com>
2024-06-12 02:57:14 -0500

    add endpoint movies api

--- Peliculas/peliculas.js
+++ Peliculas/peliculas.js
@@ -1,20 +1,47 @@
-const url = 'https://streaming-availability.p.rapidapi.com/shows/%7Btype%7D/%7Bid%7D';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '5a1de45b35msh28c5a2f63c8e81ap13c4d9jsna15abbf2c933',
-		'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
-	}
-};
+function fetchMoviesApi() {
+    const movie = "coco";
+    const url = `https://movie-database-alternative.p.rapidapi.com/?s=${movie}&r=json&page=1`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '5a1de45b35msh28c5a2f63c8e81ap13c4d9jsna15abbf2c933',
+            'x-rapidapi-host': 'movie-database-alternative.p.rapidapi.com'
+        }
+    };
 
-async function fetchDate(){
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
+    fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(response => {
+            if (response.Search) {
+                console.log(response)
+                displayMovies(response.Search);
+            } else {
+                console.error('No movies found');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-fetchDate();
+function displayMovies(movies) {
+    const main = document.querySelector('#movie-container');
+    main.innerHTML = ''; // Limpiar contenedor antes de agregar nuevas películas
+
+    movies.forEach(movie => {
+        const article = document.createRange().createContextualFragment(`
+            <article>
+                <img src="${movie.Poster}" alt="${movie.Title}">
+            </article>
+        `);
+        main.append(article);
+    });
+}
+
+// Llama a la función fetchMoviesApi al cargar la página
+document.addEventListener('DOMContentLoaded', fetchMoviesApi);
